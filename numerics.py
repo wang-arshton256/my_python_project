c1922c3f9ce8564ca4e1b157d8401cd925e1cdb8
Wangutusi Arshton <wangarshton@gmail.com>
2023-09-03 03:42:30 +0300

    numerics

---
+++ numerics.py
@@ -0,0 +1,19 @@
+# 1. Addition                   (+)
+# 2. Subtraction                (-)
+# 3. Multiplication             (*)
+# 4. Division or quotient       (/)
+# 5. Floored quotient           (//)
+# 6. Absolute value             (abs)
+# 7. Remainder of               (%)
+
+print("20 days are minutes")
+print(20 * 24 * 60)
+
+#For string concatenation we use {} and letter "f" which means format should be added to the beginning of the string or + str() +. 
+#Any data added to the brackets is treated as a string.
+
+print(f"20 days are {20 * 24 *60} minutes")
+print(f"50 days are {50 * 24 *60} minutes")
+print(f"100 days are {100 * 24 *60} minutes")
+print(f"150 days are {150 * 24 *60} minutes")
+print(f"366 days are {366 * 24 *60} minutes")
