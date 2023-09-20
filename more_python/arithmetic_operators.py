b3a02549d06cc8d7cf4d261b037e8548a6d4d52b
Wangutusi Arshton <wangarshton@gmail.com>
2023-09-21 02:25:34 +0300

    arithmetic operators

---
+++ more_python/arithmetic_operators.py
@@ -0,0 +1,33 @@
+# Python arithmetic operators
+# + Addition
+# - Subtraction
+# * Multiplication
+# / Division
+# % Modulus
+# ** Exponent
+
+# Addition of two values
+x = 34
+y = 79
+result = x + y
+print (result, '\n')
+
+# Multiplication of monthly income values
+
+Salary = 180000
+months_in_a_year = 12
+pay = 180000 * 12
+
+print (pay)
+
+
+# Order of operations
+# 'B' brackets
+# 'E' exponential
+# 'D' division
+# 'M' multiplication
+# 'A' addition
+# 'S' subtraction
+
+num = input('Enter number: ') # input returns all value entered as strings.  To covert numbers and return them as integers , we must add the int data type to the print function.
+print(int(num) - 3)
