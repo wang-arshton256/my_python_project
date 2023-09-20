2dd7e78de0b6cb845660d167891228d2f9896ade
Wangutusi Arshton <wangarshton@gmail.com>
2023-09-20 23:47:33 +0300

    user input

---
+++ more_python/user_input1.py
@@ -0,0 +1,6 @@
+#input function is used for capturing user input.
+name = input('name: ')
+age = input('age: ')
+
+#Print function is used for printing output.
+print('Hello', name, 'you are', age, 'years old')
